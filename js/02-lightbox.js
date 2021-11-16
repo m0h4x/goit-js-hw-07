db63f0daab23f5d579986c2368c31ae99b8d1fe2
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-16 09:27:43 +0100

    simpleLightBox
    simpleLightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+let galEl = document.querySelector('.gallery')
+
+const picsList = galleryItems
+    .map(
+        pic => `<a class="gallery__item" href="${pic.original}">
+        <img class="gallery_image" src=${pic.preview} alt=${pic.description} />
+            </a>`, 
+)
+    .join('')
+
+galEl.insertAdjacentHTML('beforeend', picsList)
+
+for (let i = 0; i < document.getElementsByClassName('gallery__item').length; i++)
+{
+    document.getElementsByClassName('gallery__item')[i].addEventListener('click', function (event)
+    {
+        event.preventDefault()
+        return false
+    }
+    )
+}
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionData: 'alt',
+        animationSpeed: 255
+}
+)
